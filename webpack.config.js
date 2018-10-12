69e54f61c95e26c66aeb9b2dd850baab8c5fde41
fxia1 <fxia1@lenovo.com>
2018-10-12 10:49:00 -0400

    I got it all wrong. let's follow doc to create a separate branch for this pages.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, "docs");
+var BUILD_DIR = path.resolve(__dirname, "dist");
 var APP_DIR = path.resolve(__dirname, 'src');
 
 // Boring CommonJS JavaScript
